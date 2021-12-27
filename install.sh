26c4afedbe04a2b6d8c678078b9f1e2fffe6a93e
hyvi.tan <hyvi.tan@gmail.com>
2021-12-27 15:42:24 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -2,23 +2,31 @@
 # Auth: hyvitan
 
 
+# 安装 nvim python3 插件的支持
 python3 -m pip install --user --upgrade pynvim 
 
 brew install jq
 
 brew install rg
 
+# 安装 lsp server 
 
+# golang 
 go install github.com/nametake/golangci-lint-langserver@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
 
-
+# lua
 brew install lua-language-server 
 
+# python
+pip3 install pyright
+
+# 安装中文输入法切换
 curl -o /usr/local/bin/xkbswitch https://raw.githubusercontent.com/myshov/xkbswitch-macosx/master/bin/xkbswitch
 
 chmod a+x /usr/local/bin/xkbswitch
 
-pip3 install pyright
-
+# 安装 fzf
+brew install fzf
+$(brew --prefix)/opt/fzf/install
 
