d7d2e27e3792df8289bb281cca4199ba58bc906b
Hyvi <hvyi.tan@gmail.com>
2024-07-18 01:14:45 +0000

    feat: fix default mappings for copilot <TAB>

--- .config/nvim/lua/copilot.lua
+++ .config/nvim/lua/copilot.lua
@@ -25,7 +25,43 @@ copilot_chat.setup({
     vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
   end,
   event = "VeryLazy",
+  -- default mappings
+  mappings = {
+    complete = {
+      -- detail = 'Use @<Tab> or /<Tab> for options.',
+      insert ='',
+    },
+    close = {
+      normal = 'q',
+      insert = '<C-c>'
+    },
+    reset = {
+      normal ='<C-l>',
+      insert = '<C-l>'
+    },
+    submit_prompt = {
+      normal = '<CR>',
+      insert = '<C-s>'
+    },
+    accept_diff = {
+      normal = '<C-y>',
+      insert = '<C-y>'
+    },
+    yank_diff = {
+      normal = 'gy',
+    },
+    show_diff = {
+      normal = 'gd'
+    },
+    show_system_prompt = {
+      normal = 'gp'
+    },
+    show_user_selection = {
+      normal = 'gs'
+    },
+  },
 })
 
 vim.keymap.set({ 'n', 'v' }, '<leader>ah', pick(actions.help_actions), { desc = 'AI Help Actions' })
 vim.keymap.set({ 'n', 'v' }, '<leader>ap', pick(actions.prompt_actions), { desc = 'AI Prompt Actions' })
+
