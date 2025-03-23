291c9e20e0b7b08af195c30045543970afd83b81
Hyvi <hvyi.tan@gmail.com>
2025-03-23 03:55:57 +0000

    add nvim

--- .util/before.zsh
+++ .util/before.zsh
@@ -93,7 +93,6 @@ plugins=(
   web-search
   docker-compose 
   zsh-autosuggestions
-  zsh-syntax-highlighting
 )
 
 source $ZSH/oh-my-zsh.sh
@@ -152,7 +151,6 @@ export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 alias tsm="transmission-remote"
 alias n='nvim'
-alias ssheks="ssh -i  ~/Documents/eks-pc-cloud/remote-ubuntu-sa.cer  ubuntu@ec2-13-214-156-161.ap-southeast-1.compute.amazonaws.com"
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
